f9e8755a9f3ed8575037fd4b5ed36d321151e97c
Andressa <andressajucoski@gmail.com>
2021-07-28 21:42:13 -0300

    adds react-material-icon css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '@material/react-text-field/dist/text-field.css';
+import '@material/react-material-icon/dist/material-icon.css';
 
 ReactDOM.render(
   <React.StrictMode>
