c01c412e5513689a18a35f4e09142f8cfd691528
Andressa <andressajucoski@gmail.com>
2021-07-29 19:50:52 -0300

    RestaurantDetails component

---
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Details from './RestaurantDetails.style';
+
+const RestaurantDetails = () => {
+  return (
+    <Details>
+      detalhes do restaurante selecionado
+    </Details>
+  );
+}
+
+export default RestaurantDetails;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import RestaurantCard from '../../components/RestaurantCard/RestaurantCard';
+import RestaurantDetails from '../../components/RestaurantDetails/RestaurantDetails';
 
 import { 
   Content, 
@@ -12,22 +13,28 @@ import {
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
+  const [showDetails, setShowDetails] = useState(false);
 
   return (
     <Content>
       <SideContainer>
-        <TextField
-          label='Search'
-          outlined
-          trailingIcon={<MaterialIcon role="button" icon="search" />}
-        >
-          <Input
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-        </TextField>
+        { !showDetails ?
+          <div>
+            <TextField
+              label='Search'
+              outlined
+              trailingIcon={<MaterialIcon role="button" icon="search" />}
+            >
+              <Input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+            </TextField>
 
-        <RestaurantCard />
+            <RestaurantCard />
+          </div> :
+          <RestaurantDetails />
+        }
       </SideContainer>
 
       <MapContainer>
