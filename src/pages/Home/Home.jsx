a2418145b3963c530f1bf61f25b4885138617fba
Andressa <andressajucoski@gmail.com>
2021-07-28 21:35:23 -0300

    main containers positioning

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
-import { Div } from './Home.style';
+import { 
+  Content, 
+  SearchContainer,
+  MapContainer
+} from './Home.style';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
   return (
-    <Div>
-      <TextField
-        label='Search'
-        outlined
-      >
-        <Input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-      </TextField>
-    </Div>
+    <Content>
+      <SearchContainer>
+        <TextField
+          label='Search'
+          outlined
+        >
+          <Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </TextField>
+      </SearchContainer>
+      
+      <MapContainer>
+        mapa
+      </MapContainer>
+    </Content>
   );
 }
 
--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
-export const Div = styled.div`
+export const Content = styled.div`
+  /* background: papayawhip; */
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+`;
+
+export const SearchContainer = styled.aside`
   background: ghostwhite;
-`
+  width: 100%;
+  max-width: 360px;
+`;
+
+export const MapContainer = styled.main`
+  background: lightsteelblue;
+  width: 100%;
+`;
