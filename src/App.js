b9f169cea2540c94751410734b96e6639c75bbab
Andressa <andressajucoski@gmail.com>
2021-07-31 17:07:21 -0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from './redux/store';
 import Home from './pages/Home/Home';
-
-// import { ThemeProvider } from 'styled-components';
-// import theme from './theme';
 
 import GlobalStyle from './assets/styles/global.style';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Home />
-    </>
+    </Provider>
   );
 }
 
