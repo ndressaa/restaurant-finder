5c88f200d2f0e42ccc8462a9b66a7460f3b3d36e
Andressa <andressajucoski@gmail.com>
2021-08-01 19:52:22 -0300

    cleaning setRestaurants variable before new request

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -23,6 +23,7 @@ export const MapContainer = (props) => {
 
   function searchByQuery(query) {
     const service = new google.maps.places.PlacesService(map);
+    dispatch(setRestaurants([]));
 
     const request = {
       location: map.center,
@@ -40,6 +41,7 @@ export const MapContainer = (props) => {
 
   function searchNearby(map, center) {
     const service = new google.maps.places.PlacesService(map);
+    dispatch(setRestaurants([]));
 
     const request = {
       location: center,
